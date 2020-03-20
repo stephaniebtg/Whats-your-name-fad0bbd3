f185a8d022da86e113721aff9ebd13e6d1bb3ba3
STEPHANIE BOATENG <2056840@talnet.nl>
2020-03-20 12:50:19 +0100

    Uitwerking van de exercise

---
+++ hello.php
@@ -0,0 +1,9 @@
+<?php
+$vraag1 = "Wat is je voornaam? ";
+echo $vraag1;
+$answer1 = readline();
+$vraag2 = "Wat is je achternaam? ";
+echo $vraag2;
+$answer2 = readline();
+echo "Jouw naam is: " . $answer1 ." ". $answer2;
+?>
